d6e8f543873f3b6828e4e37fe3f3415255526d20
Jonathan Yaari Barzilay <jonathan@Jonathans-MacBook-Pro.local>
2023-07-27 00:55:58 +0900

    basic socket connection handling

--- Controller.js
+++ Controller.js
@@ -1,26 +1,52 @@
 class Player {
   constructor(name) {
     this.name = name;
-    console.log("init player ", name)
+    this.text = null;
+    this.reading = null;
+    this.state = "socket pending";
+    this.socket = null;
+  }
+
+  registerSocket(socket) {
+    this.socket = socket;
+    this.state = "ready";
+    console.log(this.name + " is ready");
+
+    socket.on("disconnect", () => {
+      console.log(this.name + " disconnecte");
+    });
   }
 }
 
 class GameRoom {
-  constructor(pin) {
+  constructor(pin, admin, pref) {
+    this.admin = admin;
     this.pin = pin;
+    this.state = "lobby";
+    this.activePlayer = null;
+    this.currentRound = 0;
+    this.pref = pref;
+
     this.players = {};
-    console.log("init room ", pin);
+
+    this.registerPlayer(admin); // Registering the admin as one of the room players
+    console.log("created room " + pin + ", admin is " + admin);
   }
 
   registerPlayer(name) {
     this.players[name] = new Player(name);
   }
+
+  registerSocket(uid, socket) {
+    this.players[uid].registerSocket(socket);
+    socket.emit("test", "test");
+  }
 }
 
 class Controller {
   constructor() {
-      console.log("Initialized controller");
-      this.rooms = {1234: new GameRoom(1234)}; // Should be empty, just for debug
+    console.log("Initialized controller");
+    this.rooms = {}; // Should be empty, just for debug
   }
 
   connectPlayer(pin, player) {
@@ -34,6 +60,28 @@ class Controller {
     }
 
     this.rooms[pin].registerPlayer(player);
+  }
+
+  generatePin() {
+    const newPin = Math.round(Math.random() * 9000 + 1000);
+    if (newPin in this.rooms) {
+      return this.generatePin();
+    }
+    return newPin;
+  }
+
+  createRoom(player, rounds, selfRead, readerVisible) {
+    const pin = this.generatePin();
+    this.rooms[pin] = new GameRoom(pin, player, {
+      rounds,
+      selfRead,
+      readerVisible,
+    });
+    return pin;
+  }
+
+  registerSocket(uid, pin, socket) {
+    this.rooms[pin].registerSocket(uid, socket);
   }
 }
 
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const http = require("http");
 const { Server } = require("socket.io");
 const Controller = require("./Controller.js");
 
-const multer = require("multer");
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const multer = require("multer")();
+const cookieParse = require("cookie");
 
 const PORT = process.env.PORT || 8080;
 
@@ -16,7 +18,9 @@ const PIN = 1234;
 
 const controller = new Controller();
 
-app.use(multer().array());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer.array()); 
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
@@ -34,20 +38,41 @@ app.post("/connect", (req, res) => {
     return;
   }
 
-  res.cookie("pin", pin, {maxAge: 9999, httpOnly: true});
-  res.cookie("uid", name, {maxAge: 9999, httpOnly: true});
+  res.cookie("pin", pin, { maxAge: 9999, httpOnly: true });
+  res.cookie("uid", name, { maxAge: 9999, httpOnly: true });
 
   res.sendStatus(200);
 });
 
 app.post("/create-room", (req, res) => {
-  const { name, rounds, readerVisible, selfReading } = req.body;
+  const name = req.body.name;
+  const rounds = Number(req.body.rounds);
+  const selfRead = req.body.selfRead === "on";
+  const readerVisible = req.body.readerVisible === "on";
+  let pin;
+
+  try {
+    pin = controller.createRoom(name, rounds, selfRead, readerVisible);
+  } catch (error) {
+    res.status(400).json(String(error));
+  }
+
+  res.cookie("uid", name, { maxAge: 9999, httpOnly: true });
+  res.cookie("pin", pin, { maxAge: 9999, httpOnly: true });
+  res.sendStatus(200);
 });
 
 io.on("connection", (socket) => {
-  socket.on("disconnect", function () {
-    console.log("user disconnected");
-  });
+  const cookie = cookieParse.parse(socket.handshake.headers.cookie);
+  const pin = cookie.pin;
+  const uid = cookie.uid;
+
+  socket.join(pin);
+  controller.registerSocket(uid, pin, socket);
+
+  // socket.on("disconnect", function () {
+  //   console.log("user disconnected");
+  // });
 });
 
 httpServer.listen(PORT, function () {
